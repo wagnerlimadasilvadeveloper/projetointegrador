df1d55d22f6af1cfe70323afa9b2c852180236d9
Wagner Lima <47577309+wagnerlimadasilvadeveloper@users.noreply.github.com>
2021-08-05 21:42:15 -0300

    pagina teste

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+app.listen(8000,()=>{
+
+    app.use(express.static('public'));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+
+    app.get('/',(req,res)=> {
+        res.sendFile(__dirname + "/views/home.html");
+    
+    });
+ 
+
+    app.get('/home',(req,res)=>{
+        res.sendFile(__dirname + "/views/home.html");
+      
+
+    });
+
+    app.get('/detalhes',(req,res)=>{
+        res.sendFile(__dirname + "/views/detalhes.html");
+      
+
+    });
+
+    app.get('/categorias',(req,res)=>{
+        res.sendFile(__dirname + "/views/categorias.html");
+      
+
+    });
+
+   
+
+    console.log('Servidor Rodando')
+});
