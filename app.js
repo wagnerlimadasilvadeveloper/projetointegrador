c48375954d6ae8eccf6cdf01dde6735ff4ceb9f7
Wagner <wagnerlima.coach@gmail.com>
2021-08-05 22:08:38 -0300

    proketo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.listen(8000,()=>{
+app.listen(8000,()=> {
 
     app.use(express.static('public'));
     app.use(express.urlencoded({ extended: false }));
